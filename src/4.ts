04d819fb1cdacb695ba87f36e331e1f743eac4e1
rydb6amery <rydb6amery@gmx.com>
2025-03-07 22:13:15 +0000

    Add 4.ts

---
+++ src/4.ts
@@ -0,0 +1,17 @@
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  declarations: [],
+  providers: [],
+  bootstrap: []
+})
+export class SchoolProject1 {}
