5232af1131154cc4a5e8ca946d0cf0a1b4da6b6e
rydb6amery <rydb6amery@gmx.com>
2025-03-08 06:12:14 +0000

    Add 5.ts

---
+++ src/5.ts
@@ -0,0 +1,9 @@
+
+const schools = [
+  {name:'Shanghai',country:'China'},
+  {name:'Beijing',country:'China'},
+  {name:'London',country:'UK'},
+  {name:'Paris',country:'France'},
+  {name:'Tokyo',country:'Japan'}];
+
+return schools;
